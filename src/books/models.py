2550c2c0d99ba2719a0170a7526063a0405a0ebf
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-26 23:46:54 +0600

    Rename 'id' to 'uid' in Book model and update __repr__ method; adjust created_at and updated_at fields for consistency

--- src/books/models.py
+++ src/books/models.py
@@ -5,7 +5,7 @@ import uuid
 
 
 class Book(SQLModel, table=True):
-    id: uuid.UUID = Field(
+    uid: uuid.UUID = Field(
         default_factory=uuid.uuid4,
         sa_column=Column(
             pg.UUID(as_uuid=True),
@@ -17,18 +17,11 @@ class Book(SQLModel, table=True):
     title: str = Field(..., index=True)
     author: str
     publisher: str
-    published_date: date           # was str
+    published_date: date           
     page_count: int
     language: str
-    created_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc),
-        nullable=False,
-    )
-    updated_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc),
-        sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)},
-        nullable=False,
-    )
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)))
 
     def __repr__(self):
-        return f"<Book id={self.id} title={self.title!r}>"
+        return f"<Book uid={self.uid} title={self.title!r}>"
