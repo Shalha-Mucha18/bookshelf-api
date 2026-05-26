43220a0a7e9db884fb4a4720f86af01a4159c085
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-26 14:24:23 +0600

    Refactor FastAPI initialization and update book router prefix

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 
-app = FastAPI()
+version = "1.0.0"
 
-app.include_router(book_router, prefix="/books")
+
+app = FastAPI(
+    version=version,
+    title="Book Catalog API",
+    description="A simple API for managing a book catalog",
+)
+
+app.include_router(book_router, prefix=f"/api/v{version}/books", tags=["Books"])
