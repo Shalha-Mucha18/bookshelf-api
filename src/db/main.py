5176a725d2d69d12eecdc31d0f65e0937c015e6e
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-26 20:54:59 +0600

    Refactor database initialization in main.py to create tables and test connection

--- src/db/main.py
+++ src/db/main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine
-
+from sqlmodel import SQLModel
+from src.books.models import Book
 from src.books.config import settings
 
 
@@ -12,6 +13,6 @@ engine = create_async_engine(
 
 async def init_db():
     async with engine.begin() as conn:
-        statement = text("SELECT 'Hello, World!'")
-        result = await conn.execute(statement)
+        await conn.run_sync(SQLModel.metadata.create_all)
+        result = await conn.execute(text("SELECT 'connection test successful!'"))
         print(result.scalar())
