084f8d9d25e77c83768c6736ae39dd307d7441bb
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-26 23:47:11 +0600

    Update Book schema to use date type for published_date and add created_at, updated_at fields

--- src/books/schema.py
+++ src/books/schema.py
@@ -1,16 +1,33 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime, date
+import uuid
+
 
 class Book(BaseModel):
+    uid: uuid.UUID
     title: str
     author: str
     publisher: str
-    published_date: str
+    published_date: date
     page_count: int
     language: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    published_date: date
+    page_count: int
+    language: str
+
 
 class BookUpdate(BaseModel):
     title: Optional[str] = None
     author: Optional[str] = None
+    publisher: Optional[str] = None
     page_count: Optional[int] = None
     language: Optional[str] = None
