acedc2db9fc279e7578fceede5503c31139c9bd2
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-29 19:54:29 +0600

    addedd token schemas model

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -20,3 +20,9 @@ class UserModel(BaseModel):
     created_at: datetime
 
     model_config = {"from_attributes": True}
+
+
+
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)      
