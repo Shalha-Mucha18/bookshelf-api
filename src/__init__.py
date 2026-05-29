cd56a41dd1fffaa8d7c35d5e03ffa33cb389a36d
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-29 15:11:20 +0600

    Add authentication router to the FastAPI application

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.books.routes import book_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
+from src.auth.routers import auth
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -22,3 +23,4 @@ app = FastAPI(
 )
 
 app.include_router(book_router, prefix=f"/api/v{version}/books", tags=["Books"])
+app.include_router(auth, prefix=f"/api/v{version}/auth", tags=["Auth"])
