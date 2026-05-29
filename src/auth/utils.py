c0e3e520924c2e5a2f030e908a7f91687d33d651
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-29 19:53:52 +0600

    added  jwt token based function

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,11 @@
 from passlib.context import CryptContext
+import jwt
+import uuid
+from datetime import datetime, timedelta
+from src.books.config import settings as Config
+
+
+ACCESS_TOKEN_EXPIRY = 20 * 60  # 20 minutes
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +16,33 @@ def generated_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False) -> str:
+
+    payload = {}
+
+    payload["user"] = user_data
+    payload["exp"] = datetime.now() + (
+        expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+    )
+    payload["jti"] = str(uuid.uuid4())
+
+    payload["refresh"] = refresh
+    
+    token = jwt.encode(
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
+    )
+
+    return token
+
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, key=Config.JWT_SECRET, algorithms=[Config.JWT_ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token has expired")
+    except jwt.InvalidTokenError:
+        raise Exception("Invalid token")
+
+        
