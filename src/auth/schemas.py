1c1e385f2bb7cd38e6fc1f28a48304a88e9c5e37
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-29 15:11:44 +0600

    Refactor UserCreateModel and UserModel schemas for improved clarity and consistency

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -5,8 +5,18 @@ import uuid
 
 
 class UserCreateModel(BaseModel):
+    username: str = Field(max_length=50)
+    email: str = Field(max_length=100)
+    first_name: str = Field(max_length=50)
+    last_name: str = Field(max_length=50)
+    password: str = Field(min_length=8, max_length=128)
 
-    user_name: str = Field(max_length=50, example="John Doe")
-    email: str = Field(max_length=100, example="john.doe@example.com")
-    password: str = Field(min_length=8, max_length=128, example="password123")
 
+class UserModel(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    created_at: datetime
+
+    model_config = {"from_attributes": True}
