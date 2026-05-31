31e57d2096a9a749f795853083cea7b420c74f4d
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-05-31 23:54:23 +0600

    feat(auth): add RoleChecker dependency and fix get_current_user

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,9 +1,17 @@
 from fastapi.security import HTTPBearer
 from .utils import decode_access_token
-from fastapi import Request
+from fastapi import Request, Depends
+from src.db.main import get_session
 from fastapi import HTTPException
 from fastapi.security.http import HTTPAuthorizationCredentials
 from src.db.redis import token_in_blocklist
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.auth.service import AuthService
+from typing import List, Any
+from src.auth.models import User
+from src.auth.exceptions import AccountNotVerified, InsufficientPermission
+
+user_service = AuthService()
 
 class TokenScheme(HTTPBearer):
      
@@ -49,4 +57,24 @@ class RefreshTokenScheme(TokenScheme):
 
 
 
-     
+async def get_current_user(
+    token_details: dict = Depends(AccessTokenScheme()),
+    session: AsyncSession = Depends(get_session),
+):
+    user_email = token_details["user"]["email"]
+
+    user = await user_service.get_user_by_mail(session, user_email)
+
+    return user
+
+class RoleChecker:
+    def __init__(self, allowed_roles: List[str]) -> None:
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
+        if not current_user.is_verified:
+            raise AccountNotVerified()
+        if current_user.role in self.allowed_roles:
+            return True
+
+        raise InsufficientPermission()
