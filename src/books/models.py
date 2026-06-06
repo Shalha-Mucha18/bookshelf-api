a1d4bf481069d47f7cbbe2d1f725c74ca52ae962
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-06-06 22:20:49 +0600

    feat(models): add user_id field and relationship to Book model

--- src/books/models.py
+++ src/books/models.py
@@ -1,7 +1,13 @@
 from datetime import datetime, date, timezone
 from sqlmodel import SQLModel, Field, Column
+from sqlalchemy import ForeignKey
 import sqlalchemy.dialects.postgresql as pg
 import uuid
+from typing import TYPE_CHECKING, Optional
+from sqlmodel import Relationship
+
+if TYPE_CHECKING:
+    from src.auth.models import User
 
 
 class Book(SQLModel, table=True):
@@ -19,9 +25,18 @@ class Book(SQLModel, table=True):
     publisher: str
     published_date: date           
     page_count: int
+    user_id: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True),
+            ForeignKey("user.uid"),
+            nullable=False,
+        )
+    )
     language: str
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)))
-
+    user: Optional["User"] = Relationship(
+        back_populates="books", sa_relationship_kwargs={"lazy": "selectin"}
+    )
     def __repr__(self):
         return f"<Book uid={self.uid} title={self.title!r}>"
