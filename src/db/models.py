cf65dc627785a71454589a360162225538c527d9
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-06-06 23:38:46 +0600

    feat(models): add Review model and establish relationships with User and Book models

--- src/db/models.py
+++ src/db/models.py
@@ -29,6 +29,7 @@ class User(SQLModel, table=True):
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)))
 
     books: List["Book"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
+    reviews: List["Review"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
 
 
     def __repr__(self):
@@ -63,7 +64,33 @@ class Book(SQLModel, table=True):
     user: Optional["User"] = Relationship(
         back_populates="books", sa_relationship_kwargs={"lazy": "selectin"}
     )
+    reviews: List["Review"] = Relationship(
+        back_populates="book", sa_relationship_kwargs={"lazy": "selectin"}
+    )
     def __repr__(self):
         return f"<Book uid={self.uid} title={self.title!r}>"
+
+
+
+class Review(SQLModel, table=True):
+    __tablename__ = "reviews"
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    rating: int = Field(lt=5)
+    review_text: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="user.uid")
+    book_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="book.uid")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    user: Optional["User"] = Relationship(
+        back_populates="reviews", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+    book: Optional["Book"] = Relationship(
+        back_populates="reviews", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+    def __repr__(self):
+        return f"<Review for book {self.book_uid} by user {self.user_uid}>"
  
     
