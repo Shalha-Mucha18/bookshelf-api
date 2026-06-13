7da7b7db02d3d809a2843e45e45fc65e48194781
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-06-13 11:33:56 +0600

    feat(auth): replace HTTPException with custom exceptions for token validation

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -2,14 +2,13 @@ from fastapi.security import HTTPBearer
 from .utils import decode_access_token
 from fastapi import Request, Depends
 from src.db.main import get_session
-from fastapi import HTTPException
 from fastapi.security.http import HTTPAuthorizationCredentials
 from src.db.redis import token_in_blocklist
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.auth.service import AuthService
 from typing import List, Any
 from src.db.models import User
-from src.auth.exceptions import AccountNotVerified, InsufficientPermission
+from error import InvalidToken, AccessTokenRequired, RefreshTokenRequired, InsufficientPermission
 
 user_service = AuthService()
 
@@ -26,10 +25,10 @@ class TokenScheme(HTTPBearer):
         try:
             token_data = decode_access_token(token)
         except Exception:
-            raise HTTPException(status_code=401, detail="Invalid or expired token")
-        
+            raise InvalidToken()
+
         if await token_in_blocklist(token_data['jti']):
-            raise HTTPException(status_code=401, detail="Token has been revoked")
+            raise InvalidToken()
     
 
         self.verify_token_data(token_data)
@@ -42,18 +41,16 @@ class TokenScheme(HTTPBearer):
 
 class AccessTokenScheme(TokenScheme):
 
-
-    def verify_token_data(self,token_data:dict):
-
+    def verify_token_data(self, token_data: dict):
         if token_data and token_data['refresh']:
-            raise HTTPException(status_code=401, detail="Refresh tokens are not allowed for authentication")
+            raise AccessTokenRequired()
         
 
 class RefreshTokenScheme(TokenScheme):
 
     def verify_token_data(self, token_data: dict):
         if token_data and not token_data['refresh']:
-            raise HTTPException(status_code=401, detail="Access tokens are not allowed for refresh token operations")        
+            raise RefreshTokenRequired()        
 
 
 
@@ -72,8 +69,6 @@ class RoleChecker:
         self.allowed_roles = allowed_roles
 
     def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
-        if not current_user.is_verified:
-            raise AccountNotVerified()
         if current_user.role in self.allowed_roles:
             return True
 
