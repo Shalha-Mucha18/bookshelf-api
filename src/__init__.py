2aa97af31c2542672f2ed447e299b96bcc43ab75
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-06-13 12:34:51 +0600

    refactor(app): clean up initialization code and maintain consistent version declaration

--- src/__init__.py
+++ src/__init__.py
@@ -1,33 +1,22 @@
 from fastapi import FastAPI
-from src.books.routes import book_router
-from contextlib import asynccontextmanager
-from src.db.main import init_db
 from src.auth.routers import auth
+from src.books.routes import book_router
 from src.reviews.routers import reviews_router
 from src.tags.routes import tags_router
-from error import register_error_handlers 
-
-
-@asynccontextmanager
-async def life_span(app: FastAPI):
-
-    print("Starting up books app...")
-    await init_db()
-    yield
-    print("Shutting down books app...")
+from error import register_error_handlers
+from src.middleware import register_middleware
 
 version = "1.0.0"
 
-
 app = FastAPI(
-    version=version,
     title="Book Catalog API",
     description="A simple API for managing a book catalog",
+    version=version,
 )
 
+register_error_handlers(app)
+register_middleware(app)
 
-
-register_error_handlers(app)  
 app.include_router(book_router, prefix=f"/api/v{version}/books", tags=["Books"])
 app.include_router(auth, prefix=f"/api/v{version}/auth", tags=["Auth"])
 app.include_router(reviews_router, prefix=f"/api/v{version}/reviews", tags=["Reviews"])
