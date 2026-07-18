92c7d979a5f31a7aad800f8e1e01865088fa279d
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-07-19 00:18:26 +0900

    feat(config): add mail configuration settings to the Settings class

--- src/books/config.py
+++ src/books/config.py
@@ -1,10 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",extra="ignore")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
     database_url: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
     REDIS_HOST: str
     REDIS_PORT: int
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+    DOMAIN: str = "localhost:8000"
+
 settings = Settings()
